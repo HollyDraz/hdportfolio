218afa3ba363d649c0a999c67d7ec7ca4cbdc8c5
Holly Draz <94925959+HollyDraz@users.noreply.github.com>
2025-04-10 15:22:36 -0500

    playing with the css

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import './App.css';  
 
+
 function App() {
   return (
     <div className="App">
       <h1 className='header'>Holly Drazenovich</h1>
-      <p className='subheader'>Software Engineer</p>
-      
-      <p className='about'> About me</p>
-      <p className='projects'> Projects</p>
+
+      <p className='title'>Software Engineer</p>
+      <p className='about'>About me</p>
+      <p className='projects'>Projects</p>
       <p className='contact'>Contact me</p>
-      <p className='connect'> LinkedIn </p>
+      <p className='connect'>LinkedIn</p>
     </div>
   );
 }
