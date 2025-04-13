c8525f98f13f705d7e19e0a4b9d2048f8364102d
Holly Draz <94925959+HollyDraz@users.noreply.github.com>
2025-04-13 11:34:34 -0500

    added some styling

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -5,9 +5,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div class="cloud">
       <h1 className='header'>Holly Drazenovich</h1>
+      </div>
+     <div class="cloud2">   <p className='header'>Software Engineer</p></div>
 
-      <p className='title'>Software Engineer</p>
+
       <p className='about'>About me</p>
       <p className='projects'>Projects</p>
       <p className='contact'>Contact me</p>
