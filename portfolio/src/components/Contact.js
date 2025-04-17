0463c25ab09e4aa3b77d22830802fbf9dd07ad14
Holly Draz <94925959+HollyDraz@users.noreply.github.com>
2025-04-17 12:11:54 -0500

    added contact stying

--- portfolio/src/components/Contact.js
+++ portfolio/src/components/Contact.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import './Contact.css';
 
 function Contact() {
   return (
-    <div className="section">
+    <div className="cntsection">
       <h2>Contact Me</h2>
       <p>Let’s connect! 🌈</p>
       <a href="https://www.linkedin.com/in/hollydrazenovich" target="_blank" rel="noreferrer">LinkedIn</a>
