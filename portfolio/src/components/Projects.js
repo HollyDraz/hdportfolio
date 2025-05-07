48674fc673ec1f3a7f0019bc1fe5dd1093ff99dd
Holly Draz <94925959+HollyDraz@users.noreply.github.com>
2025-05-06 21:35:06 -0500

    added links to repos for each project

--- portfolio/src/components/Projects.js
+++ portfolio/src/components/Projects.js
@@ -7,16 +7,19 @@ function Projects() {
     <div className="prgsection">
      <div className="slideshow">
      <div class="card">
-    <h4><b>Project 1</b></h4>
-    <p>Architect & Engineer</p>
+    <h4><b>Stardew TODO</b></h4>
+    <a href='https://github.com/HollyDraz/stardew_todo'>REPO</a>
+    <p>Current project where I make a simple todo list for the popular farm game, Stardew Valley</p>
+    </div>
+     <div class="card">
+    <h4><b>Record Scratch</b></h4>
+    <a href='https://github.com/HollyDraz/record-scratch'> Repository</a>
+    <p>Full CRUD functionality app built with JS, HTML, CSS, REACT</p>
     </div>
     <div class="card">
-    <h4><b>Project 2</b></h4>
-    <p>Architect & Engineer</p>
-    </div>
-    <div class="card">
-    <h4><b>Project 3</b></h4>
-    <p>Architect & Engineer</p>
+    <h4><b>Cocktail Finder</b></h4>
+    <a href='https://github.com/HollyDraz/cocktailfinder'> REPO</a>
+    <p>Simple API based cocktail app. Built with JS, CSS, HTML</p>
     </div>
     </div>
 
